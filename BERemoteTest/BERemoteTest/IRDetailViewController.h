66ed78488bce74fe95c1a0dcbeefc1b292f0262e
Hung Ricky <ricky.houng@bomeans.com>
2016-09-21 16:18:17 +0800

    add IR control add TestAC Class complete section of create remote class

---
+++ BERemoteTest/BERemoteTest/IRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  IRDetailViewController.h
+//  BERemoteTest
+//
+//  Created by Hung Ricky on 2016/9/10.
+//  Copyright © 2016年 Hung Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IRDetailViewController : UITableViewController
+@property (nonatomic,strong) NSDictionary* wifiIrDict;
+@end
---
+++ BERemoteTest/BERemoteTest/IRSetViewController.h
@@ -0,0 +1,13 @@
+//
+//  IRSetViewController.h
+//  BERemoteTest
+//
+//  Created by Hung Ricky on 2016/9/20.
+//  Copyright © 2016年 Hung Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IRSetViewController : UIViewController
+
+@end
